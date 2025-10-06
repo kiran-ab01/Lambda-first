47b23f01b5d8b970148479d72576329f6206ed2f
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-10-06 15:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from AWS ECR + GitHub Actions!"
+    return "Hello from AWS ECR + GitHub Actions!-new release"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
